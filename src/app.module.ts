9a9d5717c529f99b81236ee8817d52f66ebc5ea2
Joshua Yoes <joshua.r.yoes@gmail.com>
2020-09-12 20:41:07 -0700

    Add Mongoose module and connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from "./app.service";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { ExerciseModule } from "./exercise/exercise.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config/dist/config.module";
+import { ConfigService } from "@nestjs/config/dist/config.service";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get("DB_URI"),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public"),
       exclude: ["/api*"],
