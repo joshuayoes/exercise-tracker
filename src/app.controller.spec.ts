adebf4d347306ef28980d7931ae6fd1c3fe19f98
Joshua Yoes <joshua.r.yoes@gmail.com>
2020-09-12 20:00:16 -0700

    Add exercise module

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    // it('should return return', () => {
-    //   expect(appController.getHello()).toBe('Hello World!');
-    // });
+    it("should be defined", () => {
+      expect(appController).toBeDefined();
+    });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
-import { ExerciseModule } from './exercise/exercise.module';
+import { ExerciseModule } from "./exercise/exercise.module";
 
 @Module({
   imports: [
