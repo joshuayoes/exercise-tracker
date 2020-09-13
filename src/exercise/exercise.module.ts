5b89b39fd9f8c5f7755321b9412d9eb74764f94b
Joshua Yoes <joshua.r.yoes@gmail.com>
2020-09-13 15:54:51 -0700

    Fix exercise service test

--- src/exercise/exercise.module.ts
+++ src/exercise/exercise.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { ExerciseService } from "./exercise.service";
 import { ExerciseController } from "./exercise.controller";
 import { MongooseModule } from "@nestjs/mongoose";
-import { User, UserSchema } from "src/schemas/user.schema";
-import { Exercise, ExerciseSchema } from "src/schemas/exercise.schema";
+import { User, UserSchema } from "../schemas/user.schema";
+import { Exercise, ExerciseSchema } from "../schemas/exercise.schema";
 import { UserService } from "../user/user.service";
 import { UserModule } from "../user/user.module";
 
--- src/exercise/exercise.service.spec.ts
+++ src/exercise/exercise.service.spec.ts
@@ -1,18 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ExerciseService } from './exercise.service';
+import { getModelToken } from "@nestjs/mongoose";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Exercise } from "../schemas/exercise.schema";
+import { User } from "../schemas/user.schema";
+import { UserService } from "../user/user.service";
+import { ExerciseService } from "./exercise.service";
 
-describe('ExerciseService', () => {
+describe("ExerciseService", () => {
   let service: ExerciseService;
+  class MongooseMock {
+    constructor(private data) {}
+
+    save = jest.fn().mockResolvedValue(this.data);
+    find = jest.fn().mockResolvedValue(this.data);
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExerciseService],
+      providers: [ExerciseService, UserService, {
+        provide: getModelToken(Exercise.name),
+        useValue: MongooseMock,
+      }, {
+        provide: getModelToken(User.name),
+        useValue: MongooseMock,
+      }],
     }).compile();
 
     service = module.get<ExerciseService>(ExerciseService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
